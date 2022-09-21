c406b65f960121dbcc58884bc9eea5ead6655e88
Documenter.jl <documenter@juliadocs.github.io>
2022-09-21 08:03:48 +0000

    build based on f0be8f8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ColorQuantization","category":"page"},{"location":"#ColorQuantization","page":"Home","title":"ColorQuantization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ColorQuantization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ColorQuantization]","category":"page"}]
+}
