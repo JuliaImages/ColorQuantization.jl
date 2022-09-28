81066d46113a01752ee2daeccb44a2bf99d94b95
Adrian Hill <adrian.hill@mailbox.org>
2022-09-28 23:34:57 +0200

    Discretize to center of cube instead of grid
    this lowers the average quantization error

--- src/uniform.jl
+++ src/uniform.jl
@@ -2,9 +2,9 @@
     UniformQuantization(n::Int)
 
 Quantize colors in RGB color space by dividing each dimension of the ``[0, 1]³``
-RGB color cube into `n` equidistant steps. This results in a grid with ``(n+1)³`` points.
-Each color in `cs` is then quantized to the closest point on the grid. Only unique colors
-are returned. The amount of output colors is therefore bounded by ``(n+1)³``.
+RGB color cube into `n` equidistant steps for a total of `n³` cubes of equal size.
+Each color in `cs` is then quantized to the center of the cube it is in.
+Only unique colors are returned. The amount of output colors is therefore bounded by `n³`.
 """
 struct UniformQuantization <: AbstractColorQuantizer
     n::Int
@@ -18,9 +18,15 @@ const UQ_COLORSPACE = RGB{Float32}
 
 (alg::UniformQuantization)(cs) = alg(convert.(UQ_COLORSPACE, cs))
 function (alg::UniformQuantization)(cs::AbstractArray{UQ_COLORSPACE})
-    return unique(map(c -> _uniform_quantization(c, alg.n), cs))
+    n = Float32(alg.n)
+    return unique(map(c -> _uniform_quantization(c, n), cs))
 end
 
 function _uniform_quantization(c::UQ_COLORSPACE, n)
-  return mapc(x -> round(x * n) / n , c)
+    return mapc(x -> _uniform_quantization(x, n), c)
+end
+function _uniform_quantization(x::Float32, n::Float32)
+    x ≤ 0 && return 1 / (2 * n)
+    x ≥ 1 && return (2 * n - 1) / (2 * n)
+    return (round(x * n - 0.5f0) + 0.5f0) / n
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -29,6 +29,29 @@ algs_deterministic = Dict(
             end
         end
     end
+    @testset "UniformQuantization" begin
+        c1 = RGB(0.0, 0.3, 1.0)
+        c2 = RGB(0.49, 0.5, 0.51)
+        c3 = RGB(0.11, 0.52, 0.73)
+        cs = [c1, c2, c3]
+
+        cs1 = quantize(cs, UniformQuantization(2))
+        # Centers of cubes at: 0.25, 0.75
+        # c1 and c2 get quantized to the same color
+        @test length(cs1) == 2
+        @test cs1[1] == RGB(0.25, 0.25, 0.75)
+        @test cs1[2] == RGB(0.25, 0.75, 0.75)
+
+        cs2 = quantize(cs, UniformQuantization(4))
+        # Centers of cubes at: 0.125, 0.375, 0.625, 0.875
+
+        # Remember that `round` defaults to `RoundNearest`,
+        # which rounds to the nearest even integer.
+        # So here, `c2.g` get's rounded up instead of down
+        @test cs2[1] == RGB(0.125, 0.375, 0.875)
+        @test cs2[2] == RGB(0.375, 0.625, 0.625)
+        @test cs2[3] == RGB(0.125, 0.625, 0.625)
+    end
 
     @testset "Error messages" begin
         @test_throws ArgumentError UniformQuantization(0)
