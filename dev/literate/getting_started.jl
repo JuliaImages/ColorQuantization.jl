085ee86dbe10d3829af77db16ea9cdd6158e334e
Documenter.jl <documenter@juliadocs.github.io>
2022-11-07 21:15:54 +0000

    build based on 61e41d6

--- dev/literate/getting_started.jl
+++ dev/literate/getting_started.jl
@@ -1,15 +1,15 @@
 # # Getting started
 # ColorQuantization.jl can be applied to any iterable collection of Colorants `img`,
 # e.g. an image or any `AbstractArray`.
-# We will demonstrate ColorQuantization.jl on the common peppers test image:
+# We demonstrate ColorQuantization.jl on the peppers test image:
 using ColorQuantization
 using ColorTypes
 using TestImages
 
 img = testimage("peppers")
 
-# To extract a color palette from an image, we call [`quantize`](@ref)
-# with a [Quantization method](@ref methods_api) of our choice.
+# To extract a color palette from an image, call [`quantize`](@ref)
+# with a [quantization method](@ref methods_api) of your choice.
 
 # ## Clustering
 # Clustering methods such as `KMeansQuantization` are convenient since they allow us
@@ -27,7 +27,7 @@ quantize(img, KMeansQuantization(256))
 # to the closest points on a equidistant 3D grid.
 #
 #md # !!! note
-#md #     In constrast to clustering, here the argument `n` specifies
+#md #     In constrast to clustering, here the argument to `UniformQuantization` specifies
 #md #     the number of subdivisions of the ``[0, 1]³`` RGB color cube.
 #md #     The amount of output colors is therefore bounded by `n³`.
 quantize(img, UniformQuantization(4))
@@ -39,7 +39,7 @@ quantize(img, UniformQuantization(6))
 quantize(img, UniformQuantization(8))
 
 # # Output color type
-# When quantizing, it is also possible to specify the output color type
+# It is also possible to specify the output color type:
 colors = quantize(HSV, img, UniformQuantization(4))
 
 #
