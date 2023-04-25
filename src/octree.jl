31ed5e9ea4099dea1d0d9832d67b41900c03537e
ashwani rathee <ab669522@gmail.com>
2023-04-25 19:40:31 +0530

    relatively better but still awfully slow

--- src/octree.jl
+++ src/octree.jl
@@ -2,24 +2,15 @@
 struct OctreeQuantization <: AbstractColorQuantizer
     numcolors::Int
     function OctreeQuantization(
-        colorspace::Type{<:Colorant},
-        numcolors::Int = 256;
-        kwargs...,
+        numcolors::Int=256;
+        kwargs...
     )
-        colorspace == RGB{N0f8} || error("Octree Algorithm only supports RGB colorspace")
         return new(numcolors)
     end
 end
 
-const OCTREE_DEFAULT_COLORSPACE = RGB{N0f8}
-# Color to RGB
-# constructor for struct
-function OctreeQuantization(numcolors::Int = 256; kwargs...)
-    return OctreeQuantization(OCTREE_DEFAULT_COLORSPACE, numcolors; kwargs...)
-end
-
 function (alg::OctreeQuantization)(img::AbstractArray)
-    return octreequantisation!(img; numcolors = alg.numcolors)
+    return octreequantisation!(img; numcolors=alg.numcolors)
 end
 
 function octreequantisation!(img; numcolors = 256, precheck::Bool = false)
@@ -39,110 +30,73 @@ function octreequantisation!(img; numcolors = 256, precheck::Bool = false)
     end
 
     # step 1: creating the octree
-    root = Cell(
-        SVector(0.0, 0.0, 0.0),
-        SVector(1.0, 1.0, 1.0),
-        ["root", 0, [], RGB{N0f8}.(0.0, 0.0, 0.0), 0],
-    )
-    cases = map(
-        p -> [bitstring(UInt8(p))[6:end], 0, Vector{Int}([]), RGB{N0f8}.(0.0, 0.0, 0.0), 1],
-        1:8,
-    )
+    root = Cell(SVector(0.0, 0.0, 0.0), SVector(1.0, 1.0, 1.0), [0, [], RGB{N0f8}.(0.0, 0.0, 0.0), 0])
+    cases = map(p -> [0, Vector{Int}([]), RGB{N0f8}.(0.0, 0.0, 0.0), 1], 1:8)
     split!(root, cases)
     inds = collect(1:length(img))
 
-    function putin(root, in)
-        r, g, b = map(p -> bitstring(UInt8(p * 255)), channelview([img[in]]))
-        rgb = r[1] * g[1] * b[1]
-        # finding the entry to the tree
-        ind = 0
-        for i = 1:8
-            if (root.children[i].data[1] == rgb)
-                root.children[i].data[2] += 1
-                ind = i
-                break
+    function putin(c::Cell, i::Int, level::Int)
+        if (level == 8)
+            push!(c.data[2], i)
+            c.data[3] = img[i]
+            return
+        else
+            if (isleaf(c) == true && level <= 8)
+                cadata = map(p -> [0, Vector{Int}([]), RGB{N0f8}.(0.0, 0.0, 0.0), level + 1], 1:8)
+                split!(c, cadata)
             end
-        end
-        curr = root.children[ind]
-
-        for i = 2:8
-            cases = map(
-                p -> [
-                    bitstring(UInt8(p))[6:end],
-                    0,
-                    Vector{Int}([]),
-                    RGB{N0f8}.(0.0, 0.0, 0.0),
-                    i,
-                ],
-                1:8,
-            )
-            rgb = r[i] * g[i] * b[i]
-            if (isleaf(curr) == true && i <= 8)
-                split!(curr, cases)
-            end
-            if (i == 8)
-                for j = 1:8
-                    if (curr.children[j].data[1] == rgb)
-                        curr = curr.children[j]
-                        curr.data[2] += 1
-                        push!(curr.data[3], in)
-                        curr.data[4] = img[in]
-                        return
-                    end
-                end
-            end
-
-            # handle 1:7 cases for rgb to handle first seven bits
-            for j = 1:8
-                if (curr.children[j].data[1] == rgb)
-                    curr.children[j].data[2] += 1
-                    curr = curr.children[j]
-                    break
-                end
-            end
+            r, g, b = map(p -> bitstring(UInt8(p * 255)), channelview([img[i]]))
+            rgb = r[level] * g[level] * b[level]
+            rgb = parse(Int, rgb, base=2) + 1
+            c.children[rgb].data[1] += 1
+            putin(c.children[rgb], i, level + 1)
         end
     end
 
+    level = 1
+    root.data[1] = length(inds)
+
     # build the tree
     for i in inds
-        root.data[2] += 1
-        putin(root, i)
+        r, g, b = map(p -> bitstring(UInt8(p * 255)), channelview([img[i]]))
+        rgb = r[level] * g[level] * b[level]
+        rgb = parse(Int, rgb, base=2) + 1
+        root.children[rgb].data[1] += 1
+        putin(root.children[rgb], i, level)
     end
 
     # step 2: reducing tree to a certain number of colors
     # there is scope for improvements in allleaves as it's found again n again
     leafs = [p for p in allleaves(root)]
-    filter!(p -> !iszero(p.data[2]), leafs)
+    filter!(p -> !iszero(p.data[1]), leafs)
     tobe_reduced = leafs[1]
 
     while (length(leafs) > numcolors)
         parents = unique([parent(p) for p in leafs])
-        parents = sort(parents; by = c -> c.data[2])
+        parents = sort(parents; by = c -> c.data[1])
         tobe_reduced = parents[1]
-        # @show tobe_reduced.data
 
         for i = 1:8
-            append!(tobe_reduced.data[3], tobe_reduced.children[i].data[3])
-            tobe_reduced.data[4] +=
-                tobe_reduced.children[i].data[4] * tobe_reduced.children[i].data[2]
+            append!(tobe_reduced.data[2], tobe_reduced.children[i].data[2])
+            tobe_reduced.data[3] +=
+                tobe_reduced.children[i].data[3] * tobe_reduced.children[i].data[1]
         end
-        tobe_reduced.data[4] /= tobe_reduced.data[2]
-        tobe_reduced.children = nothing
 
-        # we don't want to do this again n again
-        leafs = [p for p in allleaves(root)]
-        filter!(p -> !iszero(p.data[2]), leafs)
+        tobe_reduced.data[3] /= tobe_reduced.data[1]
+        filter!(!in(tobe_reduced.children),leafs)
+        push!(leafs, tobe_reduced)
+        tobe_reduced.children = nothing
     end
 
     # step 3: palette formation  and quantisation now
     da = [p.data for p in leafs]
     for i in da
-        for j in i[3]
-            img[j] = i[4]
+        for j in i[2]
+            img[j] = i[3]
         end
     end
 
-    colors = [p[4] for p in da]
+    colors = [p[3] for p in da]
     return colors
 end
 
